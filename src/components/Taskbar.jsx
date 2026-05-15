359b2c5f361540905ed83f2af62c3fc1a58a99cc
Dineshraj-03 <dineshraj.tanem@gmail.com>
2026-05-15 18:18:51 +0530

    Added start menu and persistent notes system

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -9,6 +9,7 @@ function Taskbar({
   openCalculator,
 }) {
   const [time, setTime] = useState("")
+  const [isStartMenuOpen, setIsStartMenuOpen] = useState(false)
 
   useEffect(() => {
     const updateTime = () => {
@@ -30,9 +31,57 @@ function Taskbar({
   return (
     <div className="absolute bottom-0 w-full h-14 bg-zinc-800 border-t border-zinc-700 flex items-center justify-between px-4 z-50">
 
-      <button className="bg-blue-500 px-4 py-2 rounded-lg text-white font-medium hover:bg-blue-600 transition">
-        Start
-      </button>
+      <button
+  onClick={() =>
+    setIsStartMenuOpen(!isStartMenuOpen)
+  }
+  className="bg-blue-500 px-4 py-2 rounded-lg text-white font-medium hover:bg-blue-600 transition"
+>
+  Start
+</button>
+    {isStartMenuOpen && (
+      <div className="absolute bottom-16 left-4 w-64 bg-zinc-800 border border-zinc-700 rounded-2xl shadow-2xl p-4">
+
+        <h2 className="text-white text-lg font-semibold mb-4">
+          CloudNova
+        </h2>
+
+        <div className="flex flex-col gap-2">
+
+          <button
+            onClick={() => {
+              openNotes()
+              setIsStartMenuOpen(false)
+            }}
+            className="bg-zinc-700 hover:bg-zinc-600 transition text-white px-4 py-3 rounded-xl text-left"
+          >
+            📝 Notes
+          </button>
+
+          <button
+            onClick={() => {
+              openCalculator()
+              setIsStartMenuOpen(false)
+            }}
+            className="bg-zinc-700 hover:bg-zinc-600 transition text-white px-4 py-3 rounded-xl text-left"
+          >
+            🧮 Calculator
+          </button>
+
+          <button
+            onClick={() => {
+              openFiles()
+              setIsStartMenuOpen(false)
+            }}
+            className="bg-zinc-700 hover:bg-zinc-600 transition text-white px-4 py-3 rounded-xl text-left"
+          >
+            📁 Files
+          </button>
+
+        </div>
+
+      </div>
+    )}
       <div className="flex gap-2">
 
   {isNotesOpen && (
